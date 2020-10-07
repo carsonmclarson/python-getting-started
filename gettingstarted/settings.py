b40021457cfce9b1b796eb42d540151e01366249
carsonmclarson <36063338+carsonmclarson@users.noreply.github.com>
2020-10-07 16:12:48 -0700

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(
+DATABASES = {
+    'default' : { dj_database_url.config(
     engine='heroku_connect.db.backends.postgresql'
-)
+)},
+     'sqlite' : {"ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
