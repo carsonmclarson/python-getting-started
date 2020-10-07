94aa261175c4c0e209f3734c8f893ca1bce0c23a
carsonmclarson <36063338+carsonmclarson@users.noreply.github.com>
2020-10-07 16:45:17 -0700

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Greeting, User
 
 # Create your views here.
 def index(request):
@@ -17,3 +17,18 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+class SearchView(ListView):
+    model = User
+    template_name = 'index.html'
+    context_object_name = 'all_search_results'
+
+    def get_queryset(self):
+       result = super(SearchView, self).get_queryset()
+       query = self.request.GET.get('search')
+       if query:
+          postresult = User.objects.filter(title__contains=query)
+          result = postresult
+       else:
+           result = None
+       return result
