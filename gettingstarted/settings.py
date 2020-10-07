6232390fee088f0a24e5ab758f2e53acb074d48f
carsonmclarson <36063338+carsonmclarson@users.noreply.github.com>
2020-10-07 16:18:45 -0700

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 )},
      'sqlite' : {"ENGINE" : "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+                }
     }
 
 # Password validation
