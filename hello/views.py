4da078afcdff8e2c4b0e88e69a4c8c8f83e5229e
carsonmclarson <36063338+carsonmclarson@users.noreply.github.com>
2020-10-08 16:55:17 -0700

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.list import ListView
+from django.views.generic import TemplateView, ListView
 
 from .models import Greeting, User
 
