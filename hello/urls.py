2f3b54e4bdc1d139d9ec233e643dea69f619a4d1
carsonmclarson <36063338+carsonmclarson@users.noreply.github.com>
2020-10-08 17:29:33 -0700

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import HomePageView, SearchResultsView
 
 urlpatterns = [
-    path('search/', SearchResultsView.as_view(), name='search_results'),
+    path('search/', SearchResultsView.as_view(), name='search'),
     path('', HomePageView.as_view(), name='home'),
 ]
