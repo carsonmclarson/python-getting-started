9901473462f383795437bd9b40e59bde6b71aeb8
carsonmclarson <36063338+carsonmclarson@users.noreply.github.com>
2020-10-08 17:51:47 -0700

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     #path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
-    path('', include('index.urls'))
+    path('', include('hello.urls'))
 ]
